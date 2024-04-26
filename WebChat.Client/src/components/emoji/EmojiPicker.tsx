ecc43d4fa84558e96ff6c7a7df6e99eadf644cdd
phancongthanh <phancongthanhvtvpvn@gmail.com>
2024-04-26 23:23:52 +0700

    fix: Sửa hiển thị của Popover khi Picker chưa load

--- WebChat.Client/src/components/emoji/EmojiPicker.tsx
+++ WebChat.Client/src/components/emoji/EmojiPicker.tsx
@@ -1,25 +1,28 @@
-import i18n from "@emoji-mart/data/i18n/fr.json";
+import i18n from "@emoji-mart/data/i18n/vi.json";
 import data from "@emoji-mart/data/sets/14/apple.json";
 import Picker from "@emoji-mart/react";
+import { Box } from "@mui/material";
 import { Emoji } from "emoji-mart";
 import { init } from "emoji-mart";
 
-init({ data });
+init({ data, i18n, set: "apple", locale: "vi" });
 
 export type EmojiProps = typeof Emoji.Props;
 
 export default function EmojiPicker({ onSelect }: { onSelect: (emoji: EmojiProps) => void }) {
   return (
-    <Picker
-      data={data}
-      i18n={i18n}
-      skinTonePosition="search"
-      previewPosition="none"
-      theme="light"
-      set="apple"
-      locale="vi"
-      autoFocus={true}
-      onEmojiSelect={onSelect}
-    />
+    <Box minWidth="352px">
+      <Picker
+        data={data}
+        i18n={i18n}
+        skinTonePosition="search"
+        previewPosition="none"
+        theme="light"
+        set="apple"
+        locale="vi"
+        autoFocus={true}
+        onEmojiSelect={onSelect}
+      />
+    </Box>
   );
 }
